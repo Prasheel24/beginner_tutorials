28443ae97359811621fdc27ad6a9f15a0f7699a1
Prasheel Renkuntla <prasheel08bash@gmail.com>
2019-10-26 15:11:46 -0400

    Updated Talker and listener files

--- src/beginner_tutorials/src/listener.cpp
+++ src/beginner_tutorials/src/listener.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+ * Copyright (C) 2019 Prasheel Renkuntla
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -25,18 +25,16 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-// %Tag(CALLBACK)%
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
-// %EndTag(CALLBACK)%
 
 int main(int argc, char **argv)
 {
@@ -74,20 +72,16 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-// %Tag(SUBSCRIBER)%
+
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-// %EndTag(SUBSCRIBER)%
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
    * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
-// %Tag(SPIN)%
   ros::spin();
-// %EndTag(SPIN)%
-
   return 0;
 }
-// %EndTag(FULLTEXT)%
+
 
